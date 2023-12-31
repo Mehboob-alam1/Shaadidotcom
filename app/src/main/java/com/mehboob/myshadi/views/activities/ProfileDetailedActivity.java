a19f4cc15f967bb548acdd83fedae6ab166ac410
Mehboob-Alam1 <ma8122576@gmail.com>
2023-12-31 01:20:33 -0800

    added profile details bind

--- app/src/main/java/com/mehboob/myshadi/views/activities/ProfileDetailedActivity.java
+++ app/src/main/java/com/mehboob/myshadi/views/activities/ProfileDetailedActivity.java
@@ -1,6 +1,7 @@
 package com.mehboob.myshadi.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -26,11 +27,13 @@ public class ProfileDetailedActivity extends AppCompatActivity {
 
 
     private FUPViewModel fupViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileDetailedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        fupViewModel = new ViewModelProvider(this).get(FUPViewModel.class);
 
         binding.btnBackProfile.setOnClickListener(v -> {
             finish();
@@ -52,22 +55,87 @@ public class ProfileDetailedActivity extends AppCompatActivity {
             Glide.with(this).load(userMatches.getImageUrl())
                     .placeholder(R.drawable.img_no_internet)
                     .into(binding.imgProfileImageM)
-                    ;
+            ;
 
-        }catch (Exception e){
-            Log.d("Exception",e.getLocalizedMessage());
+        } catch (Exception e) {
+            Log.d("Exception", e.getLocalizedMessage());
         }
 
-        if (userMatches.isVerified()){
+        if (userMatches.isVerified()) {
             binding.imgVerifiedM.setVisibility(View.VISIBLE);
-        }
 
-        if (userMatches.getGender().equals("Male")){
+        }
+        fupViewModel.getUserProfileLiveData().observe(this,userProfileData -> {
+
+                Glide.with(this)
+                        .load(userProfileData.getImageUrl())
+                        .placeholder(R.drawable.profile)
+                        .into(binding.imgMyProfile);
+            if (userProfileData.isVerified()) {
+                binding.txtBirthDate.setText(userMatches.getDate_of_birth());
+                binding.txtBirthDate.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                binding.txtContact.setText(userMatches.getPhoneNumber());
+                binding.txtContact.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                binding.txtEmail.setText(userMatches.getEmail());
+                binding.txtEmail.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                binding.txtWorkAsWorkandWith.setText(userMatches.getWorkAs() + ", " + userMatches.getWorksWith());
+                binding.txtWorkAsWorkandWith.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                binding.txtCollegeName.setText(userMatches.getCollege());
+                binding.txtCollegeName.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+
+            }else{
+                Toast.makeText(this, "You are not verified ", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        // if this current user is verified
+
+        binding.txtContact.setText(extractPhoneNumber(userMatches.getPhoneNumber()));
+        binding.txtIncome.setText(userMatches.getIncome());
+        binding.txtProfileNameM.setText(userMatches.getFullName());
+        binding.txtProfileAgeHeightM.setText(userMatches.getDob() + " yrs," + userMatches.getHeight());
+        binding.txtProfileProfessionM.setText(userMatches.getWorkAs());
+        binding.txtCommunitySubCommunityM.setText(userMatches.getCommunity() + " ," + userMatches.getSubCommunity());
+        binding.txtCityCountryM.setText(userMatches.getCityName() + "," + userMatches.getLivingIn());
+        binding.txtAboutPerson.setText("About " + userMatches.getFullName());
+        binding.txtAboutDescPerson.setText(userMatches.getAboutMe());
+        binding.txtCreatedBySelf.setText("Created by Self");
+        binding.txtId.setText("ID: " + userMatches.getUserId());
+        binding.txtAge.setText(userMatches.getDob() + " yrs old");
+        binding.txtHeight.setText("Height- " + userMatches.getHeight());
+        binding.txtMaritalStatus.setText(userMatches.getMaritalStatus());
+        binding.txtLivesCityStateCountry.setText(userMatches.getCityName() + ", " + userMatches.getStateName() + ", " + userMatches.getLivingIn());
+        binding.txtReligionCommunity.setText(userMatches.getReligion() + ", " + userMatches.getCommunity());
+        binding.txtSubCommunity.setText(userMatches.getSubCommunity());
+        binding.txtDiet.setText(userMatches.getDiet());
+
+        if (userMatches.getGender().equals("Male")) {
             binding.txtYouAndPersonM.setText("You and him");
-        }else{
+            binding.txtYouAndPerson.setText("You and him");
+        } else {
             binding.txtYouAndPersonM.setText("You and her");
+            binding.txtYouAndPerson.setText("You and her");
         }
 
+        Glide.with(this)
+                .load(userMatches.getImageUrl())
+                .placeholder(R.drawable.profile)
+                .into(binding.imgPersonProfile);
 
+
+    }
+
+    private String extractPhoneNumber(String phoneNumber) {
+
+
+        // Extract the relevant parts of the phone number
+        String countryCode = phoneNumber.substring(0, 3);
+        String operatorCode = phoneNumber.substring(3, 6);
+        String remainingDigits = phoneNumber.substring(6);
+
+        // Create the formatted phone number
+        String formattedPhoneNumber = "+" + countryCode + operatorCode + "******";
+
+        return formattedPhoneNumber;
     }
 }
