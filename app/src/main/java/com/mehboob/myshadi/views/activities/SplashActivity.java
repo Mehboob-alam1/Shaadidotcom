5205cc9477de7938f5adf9d7963b4466fe526f2e
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-14 01:41:02 -0800

    added dark theme support and in app updates

--- app/src/main/java/com/mehboob/myshadi/views/activities/SplashActivity.java
+++ app/src/main/java/com/mehboob/myshadi/views/activities/SplashActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mehboob.myshadi.R;
+import com.mehboob.myshadi.databinding.ActivitySplashBinding;
+import com.sanojpunchihewa.updatemanager.UpdateManager;
+import com.sanojpunchihewa.updatemanager.UpdateManagerConstant;
 
 public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+    private UpdateManager mUpdateManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+      checkForAppUpdates();
+
+    }
+
+    private void checkForAppUpdates() {
+        mUpdateManager = UpdateManager.Builder(this).mode(UpdateManagerConstant.FLEXIBLE);
+        mUpdateManager.start();
     }
 }
